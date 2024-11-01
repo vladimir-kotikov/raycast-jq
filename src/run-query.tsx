e623860a475edd551a398c342f781c4a7d8310f4
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2024-11-01 09:35:14 +0200

    Support loading document from file

--- src/run-query.tsx
+++ src/run-query.tsx
@@ -1,90 +1,94 @@
-import { Action, ActionPanel, Clipboard, List, showToast, Toast } from "@raycast/api";
-import { useCachedPromise, useExec, usePromise } from "@raycast/utils";
+import { Action, ActionPanel, Clipboard, Keyboard, List } from "@raycast/api";
+import { useExec } from "@raycast/utils";
 import { useMemo, useState } from "react";
-import { ensureJq, JQ_EXEC } from "./jq";
+import { ActionPickFile as ActionPickDocument } from "./components/ActionPickFile";
+import { useJq } from "./hooks/useJq";
+
+import { readFile } from "fs/promises";
 
 // TODO: See how details are implemented in the Multitranslator extension
+// TODO: Check how it works without internet connection
+
+const parseJqError = (errorMessage: string, jsonDocument?: string) => {
+  const [error, errorDetails, ...rest] = errorMessage.split("\n").slice(1);
+  if (error.match(/syntax error/)) {
+    return `The query \`${errorDetails.trim()}\` is invalid.`;
+  }
+
+  if (error.search(/parse error/)) {
+    // jq: parse error: Invalid numeric literal at line 2, column 12
+    let message = `The document is not a valid JSON document. `;
+    const match = /line (\d+), column (\d+)/.exec(error);
+    if (match && jsonDocument) {
+      const [location, lineStr, colStr] = match;
+      const lineNo = parseInt(lineStr) - 1;
+      const colNo = parseInt(colStr) - 1;
+      const fragment = jsonDocument.split("\n")[lineNo].slice(colNo - 100, colNo + 100);
+      message += ` The error is at ${location} around \`${fragment}\``;
+    }
+    return message;
+  }
+
+  let message = "The error is unknown. ";
+  message += `The error message is:\n\n\`\`\`\n${[errorDetails, ...rest].join("\n")}\n\`\`\``;
+
+  return message;
+};
 
 const getDetailsContent = (
   isLoading: boolean,
-  documentError: Error | undefined,
   jsonDocument: string | undefined,
   queryError: Error | undefined,
-  queryResult: string | undefined
+  queryResult: string | undefined,
 ) => {
-  if (documentError) return "### Can't load JSON document 🙍‍♀️\n\n" + documentError.message;
-  if (isLoading) return "### Spinning gears... ⌛";
-  if (!jsonDocument) return "### No JSON in clipboard 🤷‍♀️\n\nCopy a JSON document to the clipboard";
-  if (queryError) return String(queryError);
-
+  if (isLoading) return "## Spinning gears... ⌛";
+  if (!jsonDocument)
+    return "## No JSON document to query 🤷‍♀️\n\nOpen (⌘ + O) or paste (⌘ + V) document from clipboard";
+  if (queryError)
+    return "## Can't query document 🙍‍♀️\n\n" + parseJqError(queryError.message, jsonDocument);
   if (queryResult) {
-    let result = queryResult
+    let result = queryResult;
+    let subResult = "";
     if (result.length > 5000) {
-      result = result.substring(0, 5000) + '\n' +
-        "... (rest of the result is truncated for performance reasons.\n" +
-        '     Use "Copy To Clipboard" action to get the full result)';
+      result = result.substring(0, 5000);
+      subResult =
+        "\n" +
+        "Rest of the result is truncated for performance reasons.\n" +
+        'Use "Copy To Clipboard" action to get the full result)';
     }
-    return "```json\n" + result + "\n```"
-  };
+    return "```json\n" + result + "\n```" + subResult;
+  }
 
   return "Type a query to see the result";
-}
+};
 
 export default function Command() {
-  const [query, setQuery] = useState(".");
+  const [query, setQuery] = useState<string>("");
+  const [jsonDocument, setJsonDocument] = useState<string>();
+  const [loadJsonDocument, pasteJsonDocument] = useMemo(() => {
+    const initDocument = (docContent?: string) => {
+      setJsonDocument(docContent);
+      setQuery(".");
+    };
+
+    const pasteJsonDocument = () => Clipboard.readText().then(initDocument);
+    const loadJsonDocument = (fileName: string) => readFile(fileName, "utf-8").then(initDocument);
+    return [loadJsonDocument, pasteJsonDocument]
+  }, [setQuery, setJsonDocument])
 
   // Check if jq is available
-  const { isLoading: isCheckingJq, data: isJqAvailable } = useCachedPromise(async () => {
-    const toast = await showToast({
-      title: "Checking jq availability",
-      style: Toast.Style.Animated,
-    });
-
-    try {
-      const version = await ensureJq();
-      toast.style = Toast.Style.Success;
-      toast.message = version;
-      setTimeout(() => toast.hide(), 1000);
-      await toast.hide();
-      return true;
-    } catch (err) {
-      toast.style = Toast.Style.Failure;
-      toast.message = String(err);
-    }
-  }, []);
-
-  // Read the JSON document from the clipboard
-  const {
-    isLoading: isLoadingDocument,
-    data: jsonDocument,
-    error: documentError
-  } = usePromise(async () => {
-    const text = await Clipboard.readText();
-    if (text === undefined) {
-      return;
-    }
-
-    try {
-      // Intentionally disable the next line to allow the JSON.parse
-      // to throw an error when clipboard is empty
-      // TODO: Try to pass to jq without parsing and read the error to determine if the input is valid
-      JSON.parse(text); // eslint-disable-line @typescript-eslint/no-unused-expressions
-      return text;
-    } catch (err) {
-      throw new Error("JSON document in in clipboard is invalid");
-    }
-  }, [], {});
+  const { isLoading: isCheckingJq, data: jqPath } = useJq();
 
   // Execute the query, when all prerequisites are met
   const {
     isLoading: isRunningQuery,
     data: queryResult,
     error: queryError,
-  } = useExec(JQ_EXEC, [query], {
+  } = useExec(jqPath!, [query], {
     input: jsonDocument,
     initialData: jsonDocument,
     keepPreviousData: true,
-    execute: !!(isJqAvailable && query && jsonDocument),
+    execute: !!(jqPath && jsonDocument && query !== ""),
     failureToastOptions: {
       title: "Query error",
       message: "Check the query syntax and try again.",
@@ -92,37 +96,54 @@ export default function Command() {
   });
 
   const listActions = useMemo(() => {
-    return (
-      <ActionPanel title="Actions">
-        {queryResult && <Action.CopyToClipboard title="Copy Result" content={queryResult} />}
-        <Action.CopyToClipboard title="Copy Query" content={query} />
-      </ActionPanel>
+    const actionPickDocument = (
+      <ActionPickDocument
+        title="Open Json file"
+        type="public.json"
+        onSelect={loadJsonDocument}
+        shortcut={Keyboard.Shortcut.Common.Open}
+      />
     );
-  }, [query, queryResult]);
 
-  const isLoading = isCheckingJq || isRunningQuery || isLoadingDocument;
+    const actionPasteDocument = (
+      <Action
+        title="Paste Json from Clipboard"
+        onAction={pasteJsonDocument}
+        shortcut={{ key: "v", modifiers: ["cmd"] }}
+      />
+    );
 
-  const detailsContent = getDetailsContent(
-    isLoading,
-    documentError,
-    jsonDocument,
-    queryError,
-    queryResult
-  );
+    const actionCopyResult = <Action.CopyToClipboard title="Copy Result" content={queryResult} />;
+    const actionCopyQuery = <Action.CopyToClipboard title="Copy Query" content={query} />;
+
+    const actions = [actionPickDocument, actionPasteDocument];
+    if (queryResult) {
+      actions.unshift(actionCopyResult, actionCopyQuery);
+    }
+
+    return <ActionPanel title="Actions">{actions}</ActionPanel>;
+  }, [query, queryResult, loadJsonDocument, pasteJsonDocument]);
+
+  const isLoading = isCheckingJq || isRunningQuery;
+  const detailsContent = getDetailsContent(isCheckingJq, jsonDocument, queryError, queryResult);
 
   return (
     <List
-      // FIXME: isLoading is not triggered on running the query
       isLoading={isLoading}
-      isShowingDetail={true}
-      // TODO: Add debounce
-      onSearchTextChange={(text) => text !== "" && setQuery(text)}
+      isShowingDetail={!!jsonDocument}
+      searchText={query}
+      onSearchTextChange={setQuery}
+      throttle={true}
       actions={listActions}
+      searchBarPlaceholder="Enter query..."
     >
-      <List.Item
-        title="Current document"
-        detail={<List.Item.Detail markdown={detailsContent} />}
-      />
+      {jsonDocument && (
+        <List.Item
+          title="Current document"
+          detail={<List.Item.Detail markdown={detailsContent} />}
+          actions={listActions}
+        />
+      )}
     </List>
   );
 }
